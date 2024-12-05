72d5df88a65f5a06fd9468f27d4a65d397407862
manikandan170890 <manikandan1708@gmail.com>
2024-12-05 20:52:42 +0530

    first commit

---
+++ src/container/HookApp.js
@@ -0,0 +1,179 @@
+import React, { useState } from "react";
+import TableData from "../common/TableData";
+import FormData from "../common/FormData";
+const HookApp = () => {
+  const [state, setState] = useState([
+    {
+      id: "",
+      firstName: "",
+      userName: "",
+      emailId: "",
+      gender: "",
+      mobileNo: "",
+
+      fieldEdit: false,
+    },
+  ]);
+  const [userDetails, setUserDetails] = useState([]);
+  // const onChangeHandler = (formData) => {
+  //     // console.log('test', name)
+  //     // let sat = state
+  //     //      setState({...sat, [name] : value })
+  //          if(formData.target.files && formData.target.files[0]) {
+  //             let name=formData.target.name
+  //             let img = URL.createObjectURL(formData.target.files[0])
+  //             setState({ [name] : formData.target.files[0] })
+  //         }else{
+  //        const { name, value } = formData.target
+  //        setState({ [name] : value })
+  //         }
+
+  // }
+  const onChangeHandler = (formData) => {
+    let sat = state;
+    if (formData.target.files && formData.target.files[0]) {
+      let name = formData.target.name;
+      let img = URL.createObjectURL(formData.target.files[0]);
+      setState({ ...sat, [name]: formData.target.files[0] });
+    } else {
+      const { name, value } = formData.target;
+      setState({ ...sat, [name]: value });
+    }
+  };
+  const editItem = (item) => {
+    setState({ fieldEdit: true, ...item });
+  };
+
+  const deleteItem = (id) => {
+    const deleteuserDetails = userDetails.filter((item) => item.id !== id);
+    setUserDetails(deleteuserDetails);
+  };
+  const resetHandler = () => {
+    valueEmpty();
+  };
+
+  const valueEmpty = () => {
+    setState({
+      id: "",
+      firstName: "",
+      lastName: "",
+      userName: "",
+      emailId: "",
+      gender: "",
+      mobileNo: "",
+      fieldEdit: false,
+      editId: "",
+    });
+  };
+
+  // const submitHandler = () => {
+  //     let newList = {
+  //         id: 1 + Math.random(),
+  //         firstName:state.firstName,
+  //         lastName:state.lastName,
+  //         userName:state.userName,
+  //         emailId:state.emailId,
+  //         gender:state.gender,
+  //         mobileNo:state.mobileNo,
+  //     }
+  //  setUserDetails([...userDetails, newList])
+  //     valueEmpty()
+  // }
+
+  const submitHandler = () => {
+    // e.preventDefault();
+
+    let newList = {
+      id: 1 + Math.random(),
+      firstName: state.firstName,
+      lastName: state.lastName,
+      userName: state.userName,
+      emailId: state.emailId,
+      gender: state.gender,
+      mobileNo: state.mobileNo,
+      profileImage: state.profileImage,
+    };
+    //userDetails.push(newList)
+    // const userDetails = [...this.state.userDetails, newList]
+    // setState({userDetails})
+    setUserDetails([...userDetails, newList]);
+    // this.setState({updateList},() => console.log(this.state.userDetails))
+    valueEmpty();
+  };
+
+  const editHandler1 = () => {
+    const oldusers = [...userDetails];
+    const id = state.id;
+    console.log("oldusers", oldusers, id);
+    oldusers.map((item) =>
+      item.id === state.id
+        ? [
+            (item.firstName = state.firstName),
+            (item.lastName = state.lastName),
+            (item.userName = state.userName),
+            (item.emailId = state.emailId),
+            (item.gender = state.gender),
+            (item.mobileNo = state.mobileNo),
+          ]
+        : oldusers
+    );
+    setUserDetails([...userDetails]);
+    valueEmpty();
+  };
+
+  const editHandler = () => {
+   // event.preventDefault();
+
+    const oldUsers = [...userDetails];
+
+    const id = state.id;
+
+    console.log("id", id);
+
+    oldUsers.map((item) =>
+      item.id === id
+        ? [
+            item.userName = state.userName,
+
+            item.firstName = state.firstName,
+
+            item.lastName = state.lastName,
+
+            item.gender = state.gender,
+
+            item.emailId = state.emailId,
+
+            item.mobileNo = state.mobileNo,
+          ]
+        : oldUsers
+    );
+
+    console.log("old", oldUsers);
+
+    setUserDetails([...userDetails]);
+    
+    valueEmpty();
+  };
+
+  return (
+    <>
+      <div style={{ margin: "5%" }}>
+        <h5>Function / Hook Component</h5>
+        <FormData
+          state={state}
+          onChangeHandler={onChangeHandler}
+          resetHandler={resetHandler}
+          submitHandler={submitHandler}
+          editHandler={editHandler}
+        />
+        <TableData
+          tab={userDetails}
+          editItem={editItem}
+          deleteItem={deleteItem}
+        />
+      </div>
+    </>
+  );
+};
+
+export default HookApp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+ import App from './container/App';
+ import HookApp from './container/HookApp';
+ {/* <HookApp/>], */}{/* <HookApp/>], */}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+<HookApp/>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    document.getElementById('root')
+)
